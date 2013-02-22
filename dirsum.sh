d1a23b1628987e5f2cfd61ab5fd031f141f8a29b
Vysakh P Pillai <vysakhpillai@gmail.com>
2013-02-22 10:47:10 +0530

    modified dirsum.sh header comments

--- dirsum.sh
+++ dirsum.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-# Script to compute rigorous recursive digest of  a directory
-# author: vysakhpillai@gmail.com
-# usage: ./dirsum <directory>
-# notes:
-#        digest will change even if file names or locations are changed
+########################################################################################################################
+# Description : Script to compute rigorous recursive digest of  a directory
+# author      : Vysakh P Pillai
+# contact     : vysakhpillai@gmail.com
+# usage       : ./dirsum <directory>
+# notes       :
+# 	          Digest will change even if file names or locations are changed
 #
-#        works even if a file name is given as input
+#	    	  Works even if a file name is given as input. However, 
+#           	  computed digest will be different from the one conputed with the actual
+#                 command since the script uses custom format
 #
-#        to change the digest computation command, change the "DIGEST" variable.
+#           	  To change the digest computation command, change the "DIGEST" variable.
 #
-#        The computed digest will be different from the one conputed with the actual
-#        command since the script uses custom format
 #
-#        Possible (tested) commands are : md5sum, sha512sum, sha256sum, sha1sum, shasum, sha384sum, sha224sum
+#                 Possible (tested) commands are : md5sum, sha512sum, sha256sum, sha1sum, shasum, sha384sum, sha224sum
+#########################################################################################################################
 
 
 DIGEST=md5sum
